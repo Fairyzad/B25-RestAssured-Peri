6a2eab565efbf121b01a7b4e605479a87dba2ab9
peri <parizat@live.com>
2022-04-19 23:27:11 -0700

    Initial commit

---
+++ src/test/java/day1/SimpleGetRequest.java
@@ -0,0 +1,34 @@
+package day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url = "http://44.201.121.105:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        //send a get request and save response inside the Response object
+        /*
+        Testing and validating REST services in Java is harder than in dynamic languages such as Ruby and Groovy.
+        REST Assured brings the simplicity of using these languages into the Java domain.
+        For example, if your HTTP server returns the following JSON at “http://localhost:8080/lotto/{id}”:
+        You can easily use REST Assured to validate interesting things from the response:
+         */
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+        //verify status code is 200
+        Assertions.assertEquals(200,response.statusCode());
+
+    }
+}
---
+++ src/test/java/day2/SpartanGetRequest.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class SpartanGetRequest {
+
+    String url = "http://44.201.121.105:8000";
+
+    /*
+        Given Accept type is application/json
+        When user send GET request to  /api/spartans end point
+        Then status code must be 200
+        And response content type must be application/json
+     */
+
+}
