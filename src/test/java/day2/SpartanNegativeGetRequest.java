0ccf74a7a2e0939c9fb4b3fdfbe76b032f9fec77
peri <parizat@live.com>
2022-04-20 09:39:18 -0700

    day2 SpartanNegativeGetRequest.java added

--- src/test/java/day2/SpartanNegativeGetRequest.java
+++ src/test/java/day2/SpartanNegativeGetRequest.java
@@ -1,5 +1,9 @@
 package day2;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
 public class SpartanNegativeGetRequest {
 
     String url = "http://44.201.121.105:8000";
@@ -10,5 +14,10 @@ public class SpartanNegativeGetRequest {
         Then status code must be 200
         And response content type must be application/json
      */
+
+    @Test
+    public void test1(){
+       Response response = RestAssured.get(url);
+    }
 
 }
