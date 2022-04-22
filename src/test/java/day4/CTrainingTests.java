62bbeec432da758a6523d8667efe2f64372de7ca
peri <parizat@live.com>
2022-04-22 08:09:00 -0700

    day4 CTrainingTests.java added

---
+++ src/test/java/day4/CTrainingTests.java
@@ -0,0 +1,12 @@
+package day4;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CTrainingTests {
+
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI ="http://api.cybertektraining.com";
+    }
+}
