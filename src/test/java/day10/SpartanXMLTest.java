ac5ebfd3ae3f3edb7f1d891e71aecbfa31c81216
peri <parizat@live.com>
2022-05-05 11:41:36 -0700

    day10 SpartanXMLTest updated

--- src/test/java/day10/SpartanXMLTest.java
+++ src/test/java/day10/SpartanXMLTest.java
@@ -1,5 +1,60 @@
 package day10;
 
-public class SpartanXMLTest {
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utilities.SpartanTestBase;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class SpartanXMLTest extends SpartanTestBase {
+
+    @Test
+    public void test1(){
+        RestAssured.given().accept(ContentType.XML)
+                .and().auth().basic("admin","admin")
+                .when().get("/api/spartans")
+                .then().statusCode(200).and().contentType("application/xml")
+                .body("List.item[0].name",is("Rodolfo"))
+                .body("List.item[0].gender",is("Male"))
+                .log().all();
+
+    }
+    @DisplayName("GET all spartan with XML")
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.XML)
+                .and()
+                .auth().basic("admin", "admin")
+                .when().get("/api/spartans");
+
+
+        //get response xml body/payload and save inside the xmlpath object
+        XmlPath xmlPath = response.xmlPath();
+
+        //get first spartan name
+        String name = xmlPath.getString("List.item[0].name");
+        System.out.println("name = " + name);
+
+        //get me 3rd spartan id number
+        int id = xmlPath.getInt("List.item[2].id");
+        System.out.println("id = " + id);
+
+        //how to get all names and save into list of String
+        List<String> names = xmlPath.getList("List.item.name");
+        System.out.println("names = " + names);
+
+
+    }
+
+
 
 }
