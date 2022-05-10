7a6876c33822afb6e88358e558757a59339f17d8
peri <parizat@live.com>
2022-05-10 10:46:26 -0700

    MethodSourceParameterizedTest.java updated

--- src/test/java/day12/MethodSourceParameterizedTest.java
+++ src/test/java/day12/MethodSourceParameterizedTest.java
@@ -1,10 +1,13 @@
 package day12;
 
+import utilities.ExcelUtil;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,19 +16,37 @@ import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 
+
 public class MethodSourceParameterizedTest {
     @ParameterizedTest
     @MethodSource("getNames")
     public void testPrintNames(String name){
         System.out.println("name = " + name);
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("getExcelData")
+    public void excelParamTest(Map<String,String> userInfo){
+        System.out.println("userInfo.get(\"firstname\") = " + userInfo.get("firstname"));
+        System.out.println("userInfo.get(\"lastname\") = " + userInfo.get("lastname"));
 
     }
 
+
+    public static List<Map<String,String>> getExcelData(){
+        ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","Q3-short");
+
+        List<Map<String,String>> dataList = vytrackFile.getDataList();
+
+        return  dataList;
+    }
+
+
     public static List<String> getNames(){
-        // you can get value from anywhere almost any type and return to your test
+        //you can get value from anywhere almost anytype and return to your test
         //DB
-        //Other API's
+        //Other APIs
         //Excel file
 
         List<String> nameList = Arrays.asList("Muhtar","Asya","Gurhan","Adam","Akbar","Aysun","Zulfikar");
