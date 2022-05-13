da80fd9c2836ab39e4cce428184a1f32ac09dd3b
peri <parizat@live.com>
2022-05-12 21:10:47 -0700

    Day3.HrTestBase.java added

--- src/test/java/utilities/HRTestBase.java
+++ src/test/java/utilities/HRTestBase.java
@@ -8,6 +8,6 @@ public class HRTestBase {
     //beforeAll is the same thing with beforeClass in testng
     @BeforeAll
     public static void init(){
-        RestAssured.baseURI = "http://52.0.107.95:1000/ords/hr";
+        RestAssured.baseURI = "http://44.201.121.105:1000/ords/hr";
     }
 }
