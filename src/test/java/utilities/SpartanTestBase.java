e2a2c7483361706fba42dff489b415e87bf3f35e
peri <parizat@live.com>
2022-05-12 21:07:53 -0700

    day12 SpartanTestBase.java added

--- src/test/java/utilities/SpartanTestBase.java
+++ src/test/java/utilities/SpartanTestBase.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class SpartanTestBase {
@@ -10,6 +11,16 @@ public class SpartanTestBase {
         @BeforeAll
         public static void init() {
             RestAssured.baseURI = "http://44.201.121.105:8000";
+
+            String dburl = "jdbc:oracle:thin:@44.201.121.105:1521:XE";
+            String dbUserName = "SP";
+            String dbPassword = "SP";
+
+            DButils.createConnection(dburl,dbUserName,dbPassword);
+        }
+        @AfterAll
+    public static void close(){
+            DButils.destroy();
         }
 
 }
